5c5c4678d80d163d561a7a624e7955d19b55e462
PorterCat <vernikov111roma@gmail.com>
2023-09-01 15:14:24 +0700

    01.09.2023 OOP 1 LW

---
+++ OOP/Laba01.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int* genRandArray(int, int);
+void print(int*);
+
+int** genRandMatrix(int, int);
+void printMatrix(int**);
+
+void task1();
+void task2();
+
+int main()
+{
+    int v;
+    while(1)
+    {
+        printf("\nEnter the task:");
+        scanf("%i", &v);
+        switch(v)
+        {
+            case 1: 
+                task1();
+                break;
+            case 2:
+                task2();
+                break;
+            default:
+                return 0;
+        }
+    }
+}
+
+void task1()
+{
+    srand(time(NULL));
+    int size = rand()%10;
+    int maxValue = 100;
+    int* arr = genRandArray(size, maxValue);
+    print(arr);
+    free(arr);
+}
+
+int* genRandArray(int size, int maxValue)
+{
+    int* arr = malloc((size+1)*sizeof(int));
+    arr[0] = size;
+    for(int i = 1; i < size+1; i++)
+    {
+        arr[i] = rand()%maxValue;
+    }
+    return arr;
+}
+
+void print(int* arr)
+{
+    printf("%i: ", arr[0]);
+    for(int i = 1; i <= arr[0]; i++)
+    {
+        printf("%i ", arr[i]);
+    }
+}
+
+void task2()
+{
+    srand(time(NULL));
+    int size = rand()%10;
+    int maxValue = 100;
+    int** matrix = genRandMatrix(size, maxValue);
+    printMatrix(matrix);
+    
+}
+
+int** genRandMatrix(int size, int maxValue)
+{
+    int** mat = malloc((size + 1) * sizeof(int*));
+    mat[0] = malloc(1 * sizeof(int));
+    mat[0][0] = size;
+    int l_size;
+
+    for(int i = 1; i < size + 1; i++)
+    {
+        l_size = rand()%10;
+        mat[i] = malloc((l_size+1) * sizeof(int));
+        mat[i][0] = l_size;
+        for(int j = 1; j < l_size + 1; j++)
+        {
+            mat[i][j] = rand()%maxValue;
+        }
+    }
+
+    return mat;
+}
+
+void printMatrix(int** matrix)
+{
+    printf("%i\n", matrix[0][0]);
+    for(int i = 1; i <= matrix[0][0]; i++)
+    {
+        printf("%i: ", matrix[i][0]);
+        for(int j = 1; j <= matrix[i][0]; j++)
+        {
+            printf("%i ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
---
+++ ProgrammingCourse/Course.cpp
@@ -0,0 +1,154 @@
+/*
+Курсовая №10
+Написать программу шифровки-дешифровки текстового файла при помощи перемешанного алфавита (символ с кодом М(i) 
+(i – порядковый номер символа) заменяется символом с кодом (М(i)+К(i)) mod 256). К(i) – ключ шифра, случайные  
+числа в диапазоне 0..255 (хранятся в отдельном файле)
+
+Developed by Andrey Mishchuk
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <time.h>
+#include <math.h>
+
+using namespace std;
+
+
+
+int data_encryption(string path)
+{
+    ifstream file;
+    file.open(path);
+
+    string line;
+
+    if (!file.is_open())
+    {
+        cout << "Error with open the file" << path << endl;
+    }
+    else
+    {
+
+        while (!file.eof())
+        {
+            file >> line;
+        }
+    }
+
+    ofstream key_file, cryptfile;
+    string key_path = "key_file_" + path;
+    key_file.open(key_path, ios::out);
+
+    string cr_path = "cryptfile_" + path;
+    cryptfile.open(cr_path, ios::out);
+
+    if (key_file.is_open())
+    {
+        cout << "Key file for <" << path << "> has been created" << endl;
+    }
+
+    if (cryptfile.is_open())
+    {
+        cout << "The crypted code is in file <" << cr_path << ">" << endl;
+    }
+
+    srand(time(0));
+
+    for (int i = 0; i < line.length(); i++)
+    {
+        int k = rand() % 255;
+        unsigned char h = (unsigned char)(line[i] + k) % 256;
+        key_file << k << endl;
+        cryptfile << h;
+    }
+
+    key_file.close();
+    cryptfile.close();
+    return 0;
+}
+
+int data_decryption(string path, string key)
+{
+    ifstream file1, file2;
+    string line; // Хранит зашифрованную строку
+    int size = 0;
+
+    file1.open(path);     
+    if (!file1.is_open())
+    {
+        cout << "Error with open the file " << path << endl;
+    }
+    else
+    {
+        while (!file1.eof())
+        {
+            file1 >> line;
+        }
+    }
+    file1.close();
+
+    const int lsize = line.length();
+
+    int* key_mas = new int[lsize];
+
+    file2.open(key);
+
+    if (!file2.is_open())
+    {
+        cout << "Error with open the file " << path << endl;
+    }
+    else
+    {
+
+        string str; 
+        unsigned char temp;
+        int i = 0;
+
+        while (!file2.eof())
+        {
+            if (i < lsize)
+            {
+                file2 >> str;
+                temp = (unsigned char)stoi(str);
+                key_mas[i] = (int)temp;
+                i++;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    file2.close();
+
+    char decrypted_str;
+
+    for (int i = 0; i < line.length(); i++)
+    {
+        if (line[i] < key_mas[i])
+        {
+            decrypted_str = (int)line[i] + 256 - key_mas[i];
+        }
+        else
+        {
+            decrypted_str = (int)line[i] - key_mas[i];
+        }
+        cout << decrypted_str;
+    }
+
+    cout << endl;
+    return 0;
+}
+
+int main(int argc, unsigned char* argv[])
+{
+    string path = "example.txt";
+    string path_for_encrypted = "cryptfile_" + path;
+    string path_for_keyfile = "key_file_" + path;
+
+    data_encryption(path);
+
+    data_decryption(path_for_encrypted, path_for_keyfile);
+}
